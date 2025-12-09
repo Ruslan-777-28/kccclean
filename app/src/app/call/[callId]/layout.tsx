a4abfb3bd8d6b963006b98c2143e24cda2b76a30
darta com <awegvg@gmail.com>
2025-12-09 21:54:14 +0000

    Створи файл:
    src/app/call/[callId]/CallPageClient.tsx
    
    І встав сюди по

--- app/src/app/call/[callId]/layout.tsx
+++ app/src/app/call/[callId]/layout.tsx
@@ -1,3 +1,7 @@
-export default function CallLayout({ children }: { children: React.ReactNode }) {
+export default function CallLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return <>{children}</>;
 }
